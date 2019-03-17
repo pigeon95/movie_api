ac9366d069afc33b0128e7f02794ac8f4fce0bfd
kacper.golebiewski@vao.pl <kacper.golebiewski@vao.pl>
2019-03-17 23:18:45 +0100

    movies seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ActorsTableSeeder::class);
-//        $this->call(MoviesTableSeeder::class);
+        $this->call(MoviesTableSeeder::class);
     }
 }
